914cc3ac56afb440739701fd6be5ae5b11aaa4f2
Wenmo Cui <wenmo_cui@berkeley.edu>
2024-02-17 02:34:37 -0800

    updated aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,8 +6,8 @@ export default function AboutMe() {
         <section className="aboutmeSection">
             <div className="aboutmeContainer">
                 <img className="backgroundImage" src={backgroundimage}/>
-                <p className="aboutmeP"> My name is Madison Eckstrom; I am a web developer specializing in front-end development located on the island of O'ahu, Hawai'i. My skills include HTML, CSS, JavaScript, API, React, and more. Outside of work you'll find me looking for good movies to watch, enjoying some sunshine, or cooking a gratifying meal. I have a positive and passionate work ethic and hope to continually impact others through my leadership and communication. I look forward to connecting with you, <br/> <br/>
-                Aloha!</p>
+                <p className="aboutmeP"> Welcome to my homepage! My name is Wenmo Cui, a 2024 graduate from UCB extension coding bootcamp. My skillsets include node, express, MongoDB, react. Looking forward to connect with fellow developers! <br/> <br/>
+                </p>
                 <img className="aboutmeImage" src={aboutmeimage}/>
             </div>
         </section>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ export default function Footer() {
         <footer>
             <ul className="footer">
                 <li>
-                    <a href="https://github.com/maddieeckstrom">check my GitHub here</a>
+                    <a href="https://github.com/latacui2050">check my GitHub here</a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/madison-eckstrom-56a725149/">check my LinkedIn here</a>
+                    <a href="https://www.linkedin.com/in/wenmo-cui-a1638369/">check my LinkedIn here</a>
                 </li>
             </ul>
         </footer>
